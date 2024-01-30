ecc9e82f97bb2e2a631a712595f5ba6e5a12a0b1
QuinntonCarter <qdevsz@gmail.com>
2024-01-29 19:01:39 -0700

    change api call to https

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -9,7 +9,7 @@ export const useLocation = (ipLocation) => {
 
   const { data, error, isLoading } = useSWR(
     shouldFetchLocation
-      ? `http://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_API_ACCESS}&q=${ipLocation}&days=5&aqi=yes&alerts=yes`
+      ? `https://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_API_ACCESS}&q=${ipLocation}&days=5&aqi=yes&alerts=yes`
       : null,
     locationFetcher
   );
